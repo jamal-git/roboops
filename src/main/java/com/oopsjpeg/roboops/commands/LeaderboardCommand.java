4d2cd5390002a78b72e2ecdffff464d55ac38a7b
oopsjpeg <jvwill00@gmail.com>
2018-07-23 01:32:05 -0400

    Added leaderboard filtering

--- src/main/java/com/oopsjpeg/roboops/commands/LeaderboardCommand.java
+++ src/main/java/com/oopsjpeg/roboops/commands/LeaderboardCommand.java
@@ -3,18 +3,27 @@ package com.oopsjpeg.roboops.commands;
 import com.oopsjpeg.roboops.commands.util.Command;
 import com.oopsjpeg.roboops.util.Leaderboard;
 import com.oopsjpeg.roboops.util.Util;
+import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 
 public class LeaderboardCommand implements Command {
 	@Override
 	public void execute(IMessage message, String alias, String[] args) {
+		IGuild guild = message.getGuild();
 		IChannel channel = message.getChannel();
 		IUser author = message.getAuthor();
 
-		Util.sendMessage(channel, "Showing a leaderboard for **" + author.getName() + "**.",
-				Leaderboard.getGlobal(Leaderboard.MOST_MONEY));
+		EmbedObject embed;
+
+		if (args.length >= 1 && args[0].equalsIgnoreCase("server"))
+			embed = Leaderboard.getGuild(Leaderboard.MOST_MONEY, guild);
+		else
+			embed = Leaderboard.getGlobal(Leaderboard.MOST_MONEY);
+
+		Util.sendMessage(channel, "Showing a leaderboard for **" + author.getName() + "**.", embed);
 	}
 
 	@Override
--- src/main/java/com/oopsjpeg/roboops/util/Leaderboard.java
+++ src/main/java/com/oopsjpeg/roboops/util/Leaderboard.java
@@ -3,10 +3,11 @@ package com.oopsjpeg.roboops.util;
 import com.oopsjpeg.roboops.Roboops;
 import com.oopsjpeg.roboops.storage.User;
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.util.EmbedBuilder;
 
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -14,18 +15,30 @@ import java.util.stream.Collectors;
 public class Leaderboard {
 	public static final int MOST_MONEY = 0;
 
-	public static EmbedObject getGlobal(int mode) {
+	public static EmbedObject get(int mode, String scope, List<User> users) {
 		List<String> board;
 
 		switch (mode) {
-			default: board = mostMoney(Roboops.getUsers());
+			default: board = mostMoney(users);
 		}
 
 		EmbedBuilder builder = new EmbedBuilder();
-		builder.appendField(Emoji.STAR2 + "" + modeName(mode) + " (Global)",
+		builder.appendField(Emoji.STAR2 + modeName(mode) + " (" + scope + ")",
 				String.join("\n", board), false);
 
 		return builder.build();
+	}
+
+	public static EmbedObject get(int mode, String scope, Collection<IUser> users) {
+		return get(mode, scope, convertUsers(users));
+	}
+
+	public static EmbedObject getGlobal(int mode) {
+		return get(mode, "Global", Roboops.getUsers());
+	}
+
+	public static EmbedObject getGuild(int mode, IGuild guild) {
+		return get(mode, guild.getName(), guild.getUsers());
 	}
 
 	public static String modeName(int mode) {
@@ -35,14 +48,22 @@ public class Leaderboard {
 	}
 
 	public static List<String> mostMoney(List<User> users) {
-		AtomicInteger position = new AtomicInteger(10);
-		List<String> board = users.stream()
-				.sorted(Comparator.comparingInt(User::getMostMoney)).limit(10)
-				.map(u -> "**#" + position.getAndDecrement() + "** - " + u.getName() + "#"
+		AtomicInteger position = new AtomicInteger();
+		return users.stream()
+				.sorted((u1, u2) -> Integer.compare(u2.getMostMoney(), u1.getMostMoney())).limit(10)
+				.map(u -> "**#" + position.incrementAndGet() + "** - " + u.getName() + "#"
 						+ u.getDiscriminator() + " with **$" + Util.comma(u.getMostMoney()) + "**")
 				.collect(Collectors.toList());
-		Collections.reverse(board);
+	}
 
-		return board;
+	public static List<String> mostMoney(Collection<IUser> users) {
+		return mostMoney(convertUsers(users));
+	}
+
+	private static List<User> convertUsers(Collection<IUser> users) {
+		return users.stream()
+				.filter(Roboops.getUsers()::contains)
+				.map(Roboops::getUser)
+				.collect(Collectors.toList());
 	}
 }
